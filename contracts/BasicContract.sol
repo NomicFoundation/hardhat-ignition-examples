e57e4f6f3b42131f7bb7a01ff217f28bf62d84a5
Patricio Palladino <email@patriciopalladino.com>
2023-09-18 19:16:18 +0000

    Correct license

--- contracts/BasicContract.sol
+++ contracts/BasicContract.sol
@@ -1,18 +1,18 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
 contract BasicContract {
-  event BasicEvent(uint eventArg);
+    event BasicEvent(uint eventArg);
 
-  receive() external payable {}
+    receive() external payable {}
 
-  function basicFunction(uint funcArg) public {
-    // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-    // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    function basicFunction(uint funcArg) public {
+        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
-    emit BasicEvent(funcArg);
-  }
+        emit BasicEvent(funcArg);
+    }
 }
